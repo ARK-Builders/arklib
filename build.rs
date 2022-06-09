c7a0d58ab390a597405f412bc556cdf1287ecc16
Jerry Wong <yuki.n@tuta.io>
2022-06-09 15:43:07 +0800

    initialize fetching libpdfium on build time

--- build.rs
+++ build.rs
@@ -1,1 +1,121 @@
-fn main() {}
+use std::{collections::HashSet, env, path::PathBuf, str::FromStr};
+
+use flate2::read::GzDecoder;
+use fs_extra::file::CopyOptions;
+
+use tar::Archive;
+use target_lexicon::{Architecture, Environment, OperatingSystem, Triple};
+const PDFIUM_VERSION: &str = "5104";
+
+fn main() {
+    let t = env::var("TARGET").unwrap();
+    let target = Triple::from_str(t.as_str()).unwrap();
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    // Avoid dublicate download
+    if !fs_extra::dir::ls(&out_dir, &HashSet::new())
+        .unwrap()
+        .items
+        .is_empty()
+    {
+        return;
+    }
+
+    let mut name = vec!["pdfium"];
+    match target.environment {
+        Environment::Android | Environment::Androideabi => {
+            name.push("android");
+            return;
+        }
+        Environment::Musl => {
+            // compile_error!("support for musl-libc is on the way")
+        }
+
+        _ => {}
+    }
+    dbg!(&name);
+    match target.operating_system {
+        OperatingSystem::Windows => name.push("win"),
+        OperatingSystem::Linux => {
+            if target.environment != Environment::Android {
+                name.push("linux")
+            }
+        }
+        OperatingSystem::Ios => name.push("ios"),
+        OperatingSystem::MacOSX {
+            major: 11,
+            minor: 0,
+            patch: 0,
+        } => name.push("mac"),
+        _ => {
+            // compile_error!("unsupported os");
+        }
+    }
+    dbg!(&name);
+    match target.architecture {
+        Architecture::Arm(_) => name.push("arm"),
+        Architecture::Aarch64(_) => name.push("arm64"),
+        Architecture::X86_32(_) => name.push("x86"),
+        Architecture::X86_64 => name.push("x64"),
+        _ => {
+            // compile_error!("unsupported arch");
+        }
+    }
+    dbg!(&name);
+    let filename = name.join("-").to_string();
+
+    // let dest_path = Path::new(&out_dir).join(&lib_os_name);
+    let url = format!(
+        "https://github.com/bblanchon/pdfium-binaries/releases/download/chromium/{}/{}.tgz",
+        PDFIUM_VERSION, filename
+    );
+
+    let request = ureq::get(url.as_str()).call().unwrap().into_reader();
+    // client
+    //     .transfer()
+    //     .write_function(|data| {
+    //         reader.extend_from_slice(data);
+    //         Ok(data.len())
+    //     })
+    //     .unwrap();
+    // client.perform().unwrap();
+    let ar = GzDecoder::new(request);
+    let mut ar = Archive::new(ar);
+    ar.unpack(&out_dir).unwrap();
+    match target.operating_system {
+        OperatingSystem::Windows => fs_extra::file::move_file(
+            PathBuf::from(&out_dir).join("bin").join("pdfium.dll"),
+            PathBuf::from(&out_dir).join("pdfium.dll"),
+            &CopyOptions::new(),
+        )
+        .unwrap(),
+        OperatingSystem::Ios
+        | OperatingSystem::MacOSX {
+            major: 11,
+            minor: 0,
+            patch: 0,
+        } => fs_extra::file::move_file(
+            PathBuf::from(&out_dir).join("bin").join("libpdfium.dylib"),
+            PathBuf::from(&out_dir).join("libpdfium.dylib"),
+            &CopyOptions::new(),
+        )
+        .unwrap(),
+        _ => fs_extra::file::move_file(
+            PathBuf::from(&out_dir).join("lib").join("libpdfium.so"),
+            PathBuf::from(&out_dir).join("libpdfium.so"),
+            &CopyOptions::new(),
+        )
+        .unwrap(),
+    };
+    // for file in ar.entries().unwrap() {
+    //     let mut file = file.unwrap();
+
+    //     if file.path().unwrap().to_str().unwrap()
+    //         == format!("{}{}", "lib/", lib_os_name.to_str().unwrap())
+    //     {
+    //         let mut dest_file = File::create(&dest_path).unwrap();
+    //         io::copy(&mut file, &mut dest_file).unwrap();
+    //     }
+    // }
+    // sleep(Duration::from_millis(10000));
+    println!("cargo:rerun-if-changed=build.rs");
+}
--- src/pdf.rs
+++ src/pdf.rs
@@ -1,45 +1,76 @@
-use image::ImageBuffer;
-use image::Rgba;
-use pathfinder_geometry::transform2d::Transform2F;
-use pathfinder_rasterize::Rasterizer;
-use pdf::file::File as PDFile;
+use std::{
+    env,
+    path::PathBuf,
+    sync::{Arc, Mutex, Once},
+};
 
-use pdf_render::{render_page, Cache, SceneBackend};
+use image::DynamicImage;
+
+use pdfium_render::prelude::*;
+
+lazy_static! {
+    static ref EVENT_QUEUE: Mutex<bool> = Mutex::new(false);
+    static ref ONCE: Once = Once::new();
+}
 
 pub enum PDFQuailty {
     High,
     Medium,
     Low,
 }
+fn initialize_pdfium() -> Box<dyn PdfiumLibraryBindings> {
+    let out_path = env::var("OUT_DIR").unwrap_or("".to_string());
 
+    let pdfium_libpath =
+        PathBuf::from(&out_path).join(Pdfium::pdfium_platform_library_name());
+    let bindings = Pdfium::bind_to_library(pdfium_libpath.display())
+        .or_else(|_| Pdfium::bind_to_system_library())
+        .unwrap();
+    bindings
+}
 pub fn render_preview_page(
     data: &[u8],
     quailty: PDFQuailty,
-) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
-    let transform = match quailty {
-        PDFQuailty::High => Transform2F::from_scale(5.),
-        PDFQuailty::Medium => Transform2F::from_scale(150. / 32.4),
-        PDFQuailty::Low => Transform2F::from_scale(1.),
-    };
-    let file = PDFile::from_data(data.to_vec()).unwrap();
-    let page = file.get_page(0).as_deref().unwrap().to_owned();
-    let mut cache = Cache::new();
-    let mut backend = SceneBackend::new(&mut cache);
-    render_page(&mut backend, &file, &page, transform)
-        .expect("cannot render page");
-    let img_raw = Rasterizer::new().rasterize(backend.finish(), None);
-    img_raw
+    // font_path: Option<String>,
+) -> DynamicImage {
+    // EVENT_QUEUE.lock().unwrap();
+
+    let render_cfg = PdfBitmapConfig::new();
+    Pdfium::new(initialize_pdfium())
+        .load_pdf_from_bytes(data, None)
+        .unwrap()
+        .pages()
+        .get(0)
+        .unwrap()
+        .get_bitmap_with_config(&render_cfg)
+        .unwrap()
+        .as_image()
 }
 
+// #[test]
+// fn test_pdf_generate() {
+//     use std::{fs::File, io::Read};
+//     let mut pdf_reader = File::open("tests/test.pdf").unwrap();
+
+//     let mut bytes = Vec::new();
+//     pdf_reader.read_to_end(&mut bytes).unwrap();
+
+//     let img = render_preview_page(bytes.as_slice(), PDFQuailty::Low, None);
+//     img.save("tests/test.png")
+//         .expect("cannot save image");
+// }
+
 #[test]
-fn test_pdf_generate() {
-    use std::{fs::File, io::Read};
-    let mut pdf_reader = File::open("tests/test.pdf").unwrap();
+fn test_multi_pdf_generate() {
+    for i in 0..2 {
+        use std::{fs::File, io::Read};
+        let mut pdf_reader = File::open("tests/test.pdf").unwrap();
 
-    let mut bytes = Vec::new();
-    pdf_reader.read_to_end(&mut bytes).unwrap();
-
-    let img = render_preview_page(bytes.as_slice(), PDFQuailty::Low);
-    img.save("tests/test.png")
-        .expect("cannot save image");
+        let mut bytes = Vec::new();
+        pdf_reader.read_to_end(&mut bytes).unwrap();
+        println!("Rendering {}", &i);
+        let img = render_preview_page(bytes.as_slice(), PDFQuailty::Low, None);
+        img.save(format!("tests/test{}.png", &i))
+            .expect("cannot save image");
+    }
 }
