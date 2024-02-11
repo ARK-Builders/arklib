612c94bcb13b94899bde31b2545e24da4022b461
Tarek <tareknaser360@gmail.com>
2024-02-11 19:00:00 +0200

    refactor(hash): replace crc32 with farmhash
    Signed-off-by: Tarek <tareknaser360@gmail.com>

--- src/index.rs
+++ src/index.rs
@@ -685,8 +685,8 @@ mod tests {
     const FILE_NAME_2: &str = "test2.txt";
     const FILE_NAME_3: &str = "test3.txt";
 
-    const HASH_1: u32 = 3817498742;
-    const HASH_2: u32 = 1804055020;
+    const HASH_1: u64 = 15951981081336312037;
+    const HASH_2: u64 = 2324796088972609235;
 
     fn get_temp_dir() -> PathBuf {
         create_dir_at(std::env::temp_dir())
